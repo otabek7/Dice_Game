31d5d3795d6a4a89e1c6baa421cf446c30c63640
Otabek <mavlonovo@tamu.edu>
2023-07-21 21:14:52 -0500

    added winner title

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ var randomImage1 = "dice" + randomNumber2 + ".png";
 var randomSource2 = "./images/" + randomImage1;
 
 document.querySelector("img.img2").setAttribute("src", randomSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
